27d3aa95ae3a349a29900da9bd66bfcbbac75c62
mmazab <mahmoud.moneeb@gmail.com>
2014-11-15 01:14:48 -0500

    featureCategories

---
+++ DBProject/src/edu/umich/td/fextractors/Feature.java
@@ -0,0 +1,19 @@
+package edu.umich.td.fextractors;
+
+
+public class Feature {
+
+	public FeatureCategory category;
+	public String featureName, featureValue;
+	
+	Feature(String name, String value, FeatureCategory type){
+		this.featureName = name;
+		this.featureValue = value;
+		this.category = type;
+	}
+	
+	  @Override
+	  public String toString() {
+		  return "(" + featureName + ", " + featureValue + ")";
+	  }
+}
